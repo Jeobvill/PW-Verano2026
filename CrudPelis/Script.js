142c9780e6401f5c1d52a9dfa4d50a9695f1b96a
Jeobvill <jeobvillaca@ittepic.edu.mx>
2026-07-23 16:53:19 -0700

    Update Script.js

--- CrudPelis/Script.js
+++ CrudPelis/Script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/peliculas";
+const API_URL = "http://api-peliculas-five.vercel.app/peliculas";
 
 const formulario = document.getElementById("formulario");
 const id = document.getElementById("id");
